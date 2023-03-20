14d22525213a64ca054b98b953e86210347fe5b3
Stefan Rompen <54891722+sRompen@users.noreply.github.com>
2023-03-20 17:15:33 +0100

    Add project files.

---
+++ BD07/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BD07.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

1cc70bee407db76913b42dba9ef50d56a0075602
Rick Downey <rick-downey@hotmail.com>
2023-04-09 21:38:19 +0200

    More changes, fixing google authentication

--- BD07/Controllers/CalendarController.cs
+++ BD07/Controllers/CalendarController.cs
@@ -8,12 +8,62 @@ using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3;
 using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Google.Apis.Util.Store;
 
 namespace BD07.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CalendarController : ControllerBase
+    public class CalendarController : Controller
     {
+        public List<string> GoogleEvents = new List<string>();
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "MyMed";
+        //GET: Home
+        public ActionResult Index()
+        {
+            CalendarEvents();
+            ViewBag.Eventlist=GoogleEvents;
+            return View(); 
+        }
+
+        public void CalendarEvents()
+        {
+            UserCredential credential;
+           
+            using (var stream = new FileStream("Credential.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted= false;
+            request.SingleEvents= true;
+            request.MaxResults = 10;
+
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events: ");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach(var eventItem in events.Items)
+                {
+                    GoogleEvents.Add(eventItem.Summary);
+                }
+            }
+        }
     }
+
+
 }
